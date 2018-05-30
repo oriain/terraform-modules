95fb8eb92adfd64ccda483ec23740bdea0d54693
Ryan Robertson <rjr.devry@gmail.com>
2018-05-30 13:50:37 -0700

    Add optional new user-data script.

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -17,7 +17,11 @@ resource "aws_launch_configuration" "example" {
   instance_type   = "${var.instance_type}"
   security_groups = ["${aws_security_group.instance.id}"]
 
-  user_data = "${data.template_file.user_data.rendered}"
+  # user_data = "${data.template_file.user_data.rendered}"
+  user_data = "${element(concat(
+    data.template_file.user_data.*.rendered,
+    data.template_file.user_data_new.*.rendered)
+    ,0)}"
 
   lifecycle {
     create_before_destroy = true
@@ -138,12 +142,24 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
+  count = "${1 - var.enable_new_user_data}"
+
   template = "${file("${path.module}/user-data.sh")}"
 
   vars {
     server_port = "${var.server_port}"
     db_address  = "${data.terraform_remote_state.db.address}"
     db_port     = "${data.terraform_remote_state.db.port}"
+  }
+}
+
+data "template_file" "user_data_new" {
+  count = "${var.enable_new_user_data}"
+
+  template = "${file("${path.module}/user-data-new.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
   }
 }
 
--- services/webserver-cluster/vars.tf
+++ services/webserver-cluster/vars.tf
@@ -30,3 +30,7 @@ variable "max_size" {
 variable "enable_autoscaling" {
   description = "If set to true, enable auto scaling."
 }
+
+variable "enable_new_user_data" {
+  description = "If set to true, use the new User Data script."
+}
